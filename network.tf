dc29690ffafc94b3b76a15cec9ffd6750f421ceb
A Nasiou <a.nasiou@infinitum.gr>
2024-07-03 08:51:32 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,6 @@
+module "network" {
+  source  = "app.terraform.io/Infinitum-Anasiou-Lab/network/azurerm"
+  version = "3.5.0"
+  # insert required variables here
+  resource_group_name = "myresourcegroup"
+}
